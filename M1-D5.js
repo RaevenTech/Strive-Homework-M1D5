82bb3e3f2f49c5f72d3aef30796e6cdf46d37c42
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-06-29 16:06:42 +0100

    Update m1-d5

--- M1-D5.js
+++ M1-D5.js
@@ -103,7 +103,9 @@ console.log(boundry(400))
  Write a function called "strivify" which accepts a string as a parameter.
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
+function strivify(string){
 
+}
 
 
 
