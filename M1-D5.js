abe5cd38cf19d642c815b9f9ddec46b99214b6ef
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-07-19 10:34:17 +0100

    startsWith()

--- M1-D5.js
+++ M1-D5.js
@@ -104,9 +104,13 @@ console.log(boundry(400))
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 function strivify(string){
-
+  if(string.startsWith("Strive")){
+    return string
+  }else {
+    return `Strive ${string}`
+  }
 }
-
+console.log(strivify("javascript is cool"))
 
 
 // function strivify(str) {
